b973969676470f944e69b5f5e455e662fde1d074
pann <shakira.pannier@soptim.de>
2024-03-13 14:57:46 +0100

    fix reset

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import {fetchQuizQuestions, QuestionState} from "./API";
 //Components
 import QuestionCard from "./components/QuestionCard";
+import {IncentiveSaying} from "./components/score";
 
 export type AnswerObject = {
     question: string;
@@ -14,20 +15,21 @@ export type AnswerObject = {
 
 const TOTAL_QUESTIONS = 10;
 const App = () => {
-    const [loading, setLoading] = useState(false)
     const [question, setQuestion] = useState<QuestionState[]>(fetchQuizQuestions())
     const [number, setNumber] = useState(0)
     const [userAnswer, setUserAnswer] = useState<AnswerObject[]>([])
     const [score, setScore] = useState(0)
-    const [gameOver, setGameOver] = useState(true)
+    const [isRunning, setIsRunning] = useState(false)
     const [userName, setUserName] = useState<string>()
+    const [isFinshed, setIsFinished] = useState(false)
 
-    //console.log(userName)
-    //console.log(question);
+    // console.log("username " + userName)
+    // console.log("isFinished " + isFinshed)
+    // console.log("gameover " + isRunning)
+    // console.log(question);
 
     const startTrivia = () => {
-        setLoading(true)
-        setGameOver(false)
+        setIsRunning(true)
 
         const newQuestions = fetchQuizQuestions();
 
@@ -35,12 +37,11 @@ const App = () => {
         setScore(0);
         setUserAnswer([]);
         setNumber(0);
-        setLoading(false);
 
     };
 
     const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (!gameOver) {
+        if (isRunning) {
             //User answer
             const answer = e.currentTarget.value;
             // Check answer against correct answer
@@ -56,64 +57,93 @@ const App = () => {
 
             };
             setUserAnswer((prev) => [...prev, answerObject])
-        }
-    console.log(userAnswer)
-    console.log(number)
-    };
-
-    const nextQuestion = () => {
-        // Move on to next question if not the last question
-        const nextQuestion = number + 1;
-
-        if (nextQuestion === TOTAL_QUESTIONS) {
-            setGameOver(true);
-        }
-    };
-
-    return (
-        <div className="App">
-            <h1>REACT QUIZ</h1>
-            {gameOver ? // userAnswer.length === TOTAL_QUESTIONS ?
-                (
-                    <div>
-                        <form>
-                            <input
-                                onChange={(e) => setUserName(e.target.value)}
-                                name="userName" type="text"/>
-                        </form>
-                        <button className="start" onClick={startTrivia}>
-                            Start
-                        </button>
-                    </div>
-                ) : null}
-            {!gameOver ? <p className="score">Score: {score} </p> : null}
-            {loading && <p>Loading Question ...</p>}
-            {!loading && !gameOver && (
-                <QuestionCard
-                    questionNr={number + 1}
-                    totalQuestion={TOTAL_QUESTIONS}
-                    question={question[number].question}
-                    answers={question[number].answers}
-                    userAnswer={userAnswer ? userAnswer[number] : undefined}
-                    callback={checkAnswer}
-                />
-            )}
-            {!gameOver && !loading && userAnswer.length === number  && number !== TOTAL_QUESTIONS  ? (
-                <button className="next" onClick={nextQuestion}>
-                    Next Question
-                </button>
-            ) : null}
-            {
-                gameOver && userAnswer.length === TOTAL_QUESTIONS ? (
-                    <div>
-                        <div>{userName}</div>
-                        <div>{score}</div>
-                    </div>
-                ) : null
+            setNumber(number + 1)
+            if (number === TOTAL_QUESTIONS - 1) {
+                setIsRunning(false)
+                setIsFinished(true)
             }
-        </div>
+        }
+        // console.log(userAnswer)
+        // console.log(number)
+    };
 
-    );
+    const resetGame = () => {
+        setIsRunning(false)
+        setIsFinished(false)
+        setScore(0)
+        setUserAnswer([])
+        setNumber(0)
+    }
+
+    const getScoreMessage = (score: number) => {
+            switch (score) {
+                case 0:
+                    return IncentiveSaying[0];
+                case 1:
+                    return IncentiveSaying[1];
+                case 2:
+                    return IncentiveSaying[2];
+                case 3:
+                    return IncentiveSaying[3];
+                case 4:
+                    return IncentiveSaying[4];
+                case 5:
+                    return IncentiveSaying[5];
+                case 6:
+                    return IncentiveSaying[6];
+                case 7:
+                    return IncentiveSaying[7];
+                case 8:
+                    return IncentiveSaying[8];
+                case 9:
+                    return IncentiveSaying[9];
+                case 10:
+                    return IncentiveSaying[10];
+                default:
+                    return "You did it!";
+            }
+        }
+
+
+            return (
+                <div className="App">
+                    <h1>REACT QUIZ</h1>
+                    {!isRunning && !isFinshed ?
+                        (
+                            <div>
+                                <form>
+                                    <input
+                                        onChange={(e) => setUserName(e.target.value)}
+                                        name="userName" type="text" value={userName}/>
+                                </form>
+                                <button className="start" onClick={startTrivia}>
+                                    Start
+                                </button>
+                            </div>
+                        ) : null}
+                    {isRunning ? <p className="score">Score: {score} </p> : null}
+                    {isRunning && !isFinshed && (
+                        <QuestionCard
+                            questionNr={number + 1}
+                            totalQuestion={TOTAL_QUESTIONS}
+                            question={question[number].question}
+                            answers={question[number].answers}
+                            userAnswer={userAnswer ? userAnswer[number] : undefined}
+                            callback={checkAnswer}
+                        />
+                    )}
+                    {!isRunning && userAnswer.length === TOTAL_QUESTIONS && isFinshed ? (
+                        <div>
+                            <div>{userName}</div>
+                            <div>{score}</div>
+                            <div>{getScoreMessage(score)}</div>
+                            <button onClick={resetGame}>Quiz erneut starten</button>
+                        </div>
+                    ) : null
+                    }
+                </div>
+
+            );
 }
 
 export default App;
